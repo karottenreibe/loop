9095f8fe9699af739dcb8b3e0b5c5d580c628e1f
karottenreibe <karottenreibe@gmail.com>
2010-07-02 10:09:00 +0200

    started parser impl

--- loop.cpp
+++ loop.cpp
@@ -8,132 +8,9 @@
 #include <map>
 #include <vector>
 #include <iostream>
+#include "lexer.cpp"
 
 using namespace llvm;
-
-// ===== lexer ================================================================
-
-enum TokenType {
-    tok_eof = -1,
-    tok_loop = -2,
-    tok_do = -3,
-    tok_end = -4,
-    tok_number = -5,
-    tok_plus = -6,
-    tok_minus = -7,
-    tok_par_open = -8,
-    tok_par_closed = -9,
-    tok_assign = -10,
-    tok_ident = -11,
-    tok_sep = -12,
-    tok_invalid = -99,
-};
-
-struct Token {
-    int type;
-    int ibuffer;
-    std::string cbuffer;
-    Token() {
-        this->type = 0;
-        this->ibuffer = 0;
-        this->cbuffer = "";
-    }
-};
-
-struct Lexer {
-    char last_char;
-
-    Lexer() {
-        this->last_char = ' ';
-    }
-
-    Token next_token() {
-        Token token;
-        while (true) {
-            // identifier := [a-z][a-z0-9]*
-            if (isalpha(last_char) || (token.type == tok_ident && isdigit(last_char))) {
-                token.type = tok_ident;
-                token.cbuffer += last_char;
-            // number := [0-9]+
-            } else if (isdigit(last_char)) {
-                token.type = tok_number;
-                token.ibuffer = (token.ibuffer * 10) + last_char - 48;
-            // +
-            } else if (last_char == '+') {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-                token.type = tok_plus;
-                last_char = getchar();
-                break;
-            // -
-            } else if (last_char == '-') {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-                token.type = tok_minus;
-                last_char = getchar();
-                break;
-            // =
-            } else if (last_char == '=') {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-                token.type = tok_assign;
-                last_char = getchar();
-                break;
-            // ;
-            } else if (last_char == ';') {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-                token.type = tok_sep;
-                last_char = getchar();
-                break;
-            // space
-            } else if (isspace(last_char)) {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-            // EOF
-            } else if (last_char == EOF) {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-                token.type = tok_eof;
-                break;
-            // invalid
-            } else {
-                // if something else has been read, we abort.
-                // otherwise we ignore
-                if (token.type != 0) {
-                    break;
-                }
-                token.type = tok_invalid;
-                token.cbuffer += last_char;
-                last_char = getchar();
-                break;
-            }
-            last_char = getchar();
-        }
-        return token;
-    }
-};
-
-
-// ===== parser ===============================================================
 
 // <expression> := <expression> ; <expression> | <assignment> | <loop>
 struct ExprAST {
@@ -167,17 +44,95 @@ struct LoopAST : public ExprAST {
     LoopAST(ExprAST arg, ExprAST b) : argument(arg), body(b) {}
 };
 
-// <parens> := (<value>)
-struct ParensAST : public ExprAST {
-    ExprAST body;
-    ParensAST(ExprAST b) : body(b) {}
-};
-
 // <assignment> := <identifier> = <value>
 struct AssignAST : public ExprAST {
     IdentifierAST identifier;
     ExprAST value;
     AssignAST(IdentifierAST ident, ExprAST val) : identifier(ident), value(val) {}
+};
+
+struct Parser {
+    Token token;
+    Lexer lexer;
+
+    Parser() {
+        eat();
+    }
+
+    Token eat() {
+        Token buf = token;
+        token = lexer.next_token();
+        return buf
+    }
+
+    ExprAST* error(const char * expected) {
+        fprintf(stderr, "error: unexpected token. expected `%s'\n", expected);
+        return NULL;
+    }
+
+    // <expression> := <expression> ; <expression> | <assignment> | <loop>
+
+    // <assignment> := <identifier> = <value>
+    ExprAST* parseAssignment() {
+        IdentifierAST ident = parseIdent();
+        if (ident == NULL) {
+            return NULL;
+        } else {
+            if (token.type != tok_assign) {
+                return error("=");
+            }
+            eat();
+            ExprAST* value = parseValue();
+            if (value == NULL) {
+                return NULL;
+            } else {
+                return new AssignAST(ident, value);
+            }
+        }
+    }
+
+    // <value> := <number> | <term> | <parens>
+    ExprAST* parseValue() {
+        // <parens>
+        if (token.type == tok_par_open) {
+            return parseParens();
+        } else if (token.type == tok_number) {
+            return parseNumber();
+        } else {
+            return parseTerm();
+        }
+    }
+
+    // <parens> := (<value>)
+    ExprAST* parseParens() {
+        eat();
+        ExprAST* body = parseValue();
+        if (body == NULL) {
+            return NULL;
+        } else {
+            if (token.type != tok_par_closed) {
+                return error(")");
+            }
+            eat();
+            return body;
+        }
+    }
+
+    // <term> := <value> + <value> | <value> - <value>
+
+    // <loop> := loop <value> do <expression> end
+
+    // <number> := [0-9]+
+    ExprAST* parseNumber() {
+        Token number = eat();
+        return new NumberAST(indent.ibuffer);
+    }
+
+    // <identifier> := [a-z][a-z0-9]*
+    ExprAST* parseIdent() {
+        Token ident = eat();
+        return new IdentifierAST(ident.cbuffer);
+    }
 };
 
 
